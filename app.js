5610e8c20b51a39add1d83ac36588c2e6e3d9fca
João Victor Sawada <joaovictorsawada@gmail.com>
2021-03-01 10:35:50 -0300

    Porta de Produção

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('./src/config/express/custom-express')
 
 const PORT = process.env.PORT || 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('\nSERVER: [Ouvindo porta 3000]')
     console.log('--------------------------------------')
 })
